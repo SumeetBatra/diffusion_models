c68bbe9e9d9f65c6e5c731cac5269adaed1e5135
Sumeet Batra <sscifi@gmail.com>
2023-03-14 09:18:13 -0700

    autoencoder base

---
+++ autoencoders/autoencoder_base.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+
+from abc import abstractmethod
+
+
+class GaussianDistribution:
+    # https://nn.labml.ai/diffusion/stable_diffusion/model/autoencoder.html
+    def __init__(self, parameters: torch.Tensor):
+        self.mean, logvar = torch.chunk(parameters, 2, dim=1)
+        self.logvar = torch.clamp(logvar, -30.0, 20.0)
+        self.var = torch.exp(self.logvar)
+        self.std = torch.exp(0.5 * self.logvar)
+        self.deterministic = False
+
+    def sample(self):
+        return self.mean + self.std * torch.randn_like(self.std)
+
+    def kl(self, other=None):
+        if self.deterministic:
+            return torch.Tensor([0.])
+        else:
+            if other is None:
+                return 0.5 * torch.sum(torch.pow(self.mean, 2)
+                                       + self.var - 1.0 - self.logvar,
+                                       dim=[1, 2, 3])
+            else:
+                return 0.5 * torch.sum(
+                    torch.pow(self.mean - other.mean, 2) / other.var
+                    + self.var / other.var - 1.0 - self.logvar + other.logvar,
+                    dim=[1, 2, 3])
+
+
+class AutoEncoderBase(nn.Module):
+    def __init__(self, emb_channels: int, z_channels: int):
+        super().__init__()
+        self.encoder: nn.Module = None
+        self.decoder: nn.Module = None
+
+        # Convolution to map from embedding space to
+        # quantized embedding space moments (mean and log variance)
+        self.quant_conv = nn.Conv2d(2 * z_channels, 2 * emb_channels, 1)
+        # Convolution to map from quantized embedding space back to
+        # embedding space
+        self.post_quant_conv = nn.Conv2d(emb_channels, z_channels, 1)
+
+    def encode(self, x: torch.Tensor):
+        assert self.encoder is not None, "Need to define a valid encoder (nn.Module)"
+        z = self.encoder(x)
+        moments = self.quant_conv(z)
+        return GaussianDistribution(moments)
+
+    def decode(self, z: torch.Tensor):
+        assert self.decoder is not None, "Need to define a valid decoder (nn.Module)"
+        z = self.post_quant_conv(z)
+        return self.decoder(z)
+
+    def forward(self, x: torch.Tensor):
+        posterior = self.encode(x)
+        z = posterior.sample()
+        out = self.decode(z)
+        return out, posterior
