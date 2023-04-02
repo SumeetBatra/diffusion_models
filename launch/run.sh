d7f6714f8387ecd7696c460b25e97b607b54aee6
Shashank <khegde@usc.edu>
2023-04-02 04:00:23 -0700

    runner from z ablation

--- launch/run.sh
+++ launch/run.sh
@@ -7,8 +7,25 @@
 
 # env setup
 seed=111
+
+
+z_c=8
+z_h=16
+tags=LDM16
+
+# z_c=4
+# z_h=32
+# tags=LDM8
+
+# z_c=3
+# z_h=64
+# tags=LDM4
+
+
 # train
 
 srun \
 python -m algorithm.train_autoencoder \
---seed $seed --use_wandb True
+--seed $seed --use_wandb True --num_epochs 200 \
+--merge_obsnorm False --wandb_tag $tags --inp_coef 1 \
+--z_channels z_c --z_height z_h
