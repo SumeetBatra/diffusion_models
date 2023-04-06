(WIP) fixes to LPIPS code. It now runs! but still need to understand/implement the scaling layer and spatial flag

algorithm/train_autoencoder.py
@@ -20,6 +20,7 @@ from attrdict import AttrDict
 from utils.brax_utils import compare_rec_to_gt_policy
 from utils.utilities import log, config_wandb
 from functools import partial
+from losses.contperceptual import LPIPS
 
 
 import scipy.stats as stats
@@ -227,6 +228,22 @@ def mse_loss_from_weights_dict(target_weights_dict: dict, rec_agents: list[Actor
     return loss, loss_info
 
 
+def agent_to_weights_dict(agents: list[Actor]):
+    '''Converts a batch of agents of type 'Actor' to a dict of batched weights'''
+    weights_dict = {}
+    for agent in agents:
+        for name, param in agent.named_parameters():
+            if name not in weights_dict:
+                weights_dict[name] = []
+            weights_dict[name].append(param)
+
+    # convert lists to torch tensors
+    for key in weights_dict.keys():
+        weights_dict[key] = torch.stack(weights_dict[key])
+
+    return weights_dict
+
+
 def train_autoencoder():
     # experiment name
     exp_name = 'autoencoder_' + datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -271,6 +288,8 @@ def train_autoencoder():
         # freeze the encoder
         for param in encoder_pretrained.parameters():
             param.requires_grad = False
+        # 'perceptual loss' using deep features
+        percept_loss = LPIPS(behavior_predictor=encoder_pretrained, spatial=False)
             
 
     optimizer = Adam(model.parameters(), lr=1e-3)
@@ -324,7 +343,8 @@ def train_autoencoder():
                 wandb.log(info)
 
         epoch_mse_loss = 0
-        epoch_kl_loss = 0        
+        epoch_kl_loss = 0
+        epoch_perceptual_loss = 0
         loss_infos = []
         for step, (policies, measures, _) in enumerate(dataloader):
             optimizer.zero_grad()
@@ -339,19 +359,11 @@ def train_autoencoder():
             loss = policy_mse_loss + args.kl_coef * kl_loss
 
             if args.perceptual_loss_coef > 0:
-                # get the features from the pretrained encoder
-                _, gt_features = encoder_pretrained(policies)
-                pred_weights_dict = {}
-                for agent in rec_policies:
-                    for name, param in agent.named_parameters():
-                        if name not in pred_weights_dict:
-                            pred_weights_dict[name] = []
-                        pred_weights_dict[name].append(param)    
-                for key, val in pred_weights_dict.items():
-                    pred_weights_dict[key] = torch.stack(val, dim=0)  
-                pred_weights_dict['actor_logstd'] = policies['actor_logstd'] 
-                _, rec_features = encoder_pretrained(pred_weights_dict)
-                perceptual_loss = F.mse_loss(gt_features, rec_features)
+                rec_weights_dict = agent_to_weights_dict(rec_policies)
+                rec_weights_dict['actor_logstd'] = policies['actor_logstd']
+                perceptual_loss = percept_loss(policies, rec_weights_dict).mean()
+
+                epoch_perceptual_loss += perceptual_loss.item()
                 loss += args.perceptual_loss_coef * perceptual_loss
 
             loss.backward()
@@ -373,9 +385,15 @@ def train_autoencoder():
 
             writer.add_scalar("Loss/mse_loss", epoch_mse_loss / len(dataloader), global_step+1)
             writer.add_scalar("Loss/kl_loss", epoch_kl_loss / len(dataloader), global_step+1)
-            wandb.log({'Loss/mse_loss': epoch_mse_loss / len(dataloader), "global_step": global_step+1})
-            wandb.log({'Loss/kl_loss': epoch_kl_loss / len(dataloader), "global_step": global_step+1})
-            wandb.log({'epoch': epoch + 1, 'global_step': global_step + 1})
+            writer.add_scalar("Loss/perceptual_loss", epoch_perceptual_loss / len(dataloader), global_step+1)
+            wandb.log({
+                # TODO: why are we dividing by the length of the dataloader?
+                'Loss/mse_loss': epoch_mse_loss / len(dataloader),
+                'Loss/kl_loss': epoch_kl_loss / len(dataloader),
+                'Loss/perceptual_loss': epoch_perceptual_loss / len(dataloader),
+                'epoch': epoch + 1,
+                'global_step': global_step + 1
+            })
             for key in avg_loss_infos.keys():
                 writer.add_scalar(f"Loss/{key}", avg_loss_infos[key], global_step+1)
                 wandb.log({f"Loss/{key}": avg_loss_infos[key], "global_step": global_step+1})

autoencoders/policy/hypernet.py
@@ -7,7 +7,6 @@ from autoencoders.autoencoder_base import AutoEncoderBase
 from models.hyper.ghn_modules import *
 from RL.actor_critic import Actor
 
-
 class HypernetAutoEncoder(AutoEncoderBase):
     def __init__(self, emb_channels: int, z_channels: int, normalize_obs: bool = False, z_height = 4):
         """
@@ -176,22 +175,29 @@ class ModelEncoder(nn.Module):
 
     def forward(self, x, get_intermediate_features=False):
         outs = []
+        features = []
         for k in range(len(self.list_of_weight_names)):
             name = self.list_of_weight_names[k]
             real_name = self.list_of_real_weight_names[k]
             if 'weight' in name:
                 out = self.cnns[name](x[real_name].unsqueeze(1))
+                if get_intermediate_features:
+                    features.append(out)
                 out = out.view(out.size(0), -1)
                 outs.append(out)
             elif 'bias' in name:
                 out = self.cnns[name](x[real_name].unsqueeze(1))
+                if get_intermediate_features:
+                    features.append(out[:, :, :, None])
                 out = out.view(out.size(0), -1)
                 outs.append(out)
             else:
                 out = self.cnns[name](x[real_name].unsqueeze(1))
+                if get_intermediate_features:
+                    features.append(out)
                 out = out.view(out.size(0), -1)
                 outs.append(out)
-        
+
         if self.obs_norm:
             out = self.cnns['rms_mean'](x['rms_mean'].unsqueeze(1))
             out = out.view(out.size(0), -1)
@@ -203,8 +209,7 @@ class ModelEncoder(nn.Module):
         x = torch.cat(outs, dim=1)
         x = self.out(x)
         if get_intermediate_features:
-            features = outs
-            features.append(x)
+            features.append(x[:, :, None, None])
             # TODO: @Shank should we also append the outputs of self.measure_out? I think
             # TODO: since self.out() is the 2nd to last layer, this should be enough?
             return features

losses/contperceptual.py
@@ -26,7 +26,7 @@ def normalize_tensor(in_feat,eps=1e-10):
 
 
 def spatial_average(in_tens, keepdim=True):
-    return in_tens.mean([2,3],keepdim=keepdim)
+    return in_tens.mean([2, 3], keepdim=keepdim)
 
 
 def upsample(in_tens, out_HW=(64,64)): # assumes scale factor is same for H and W
@@ -62,7 +62,7 @@ class LPIPS(nn.Module):
 
         num_features = len(self.behavior_predictor.cnns) + 1  # all cnns + 2nd to last dense layer
         for kk in range(num_features):
-            gt_features[kk], rec_features[kk] = normalize_tensor(gt_preds[kk]), normalize_tensor(rec_features[kk])
+            gt_features[kk], rec_features[kk] = normalize_tensor(gt_preds[kk]), normalize_tensor(rec_preds[kk])
             diffs[kk] = (gt_features[kk] - rec_features[kk])**2
 
         if self.spatial:
@@ -75,7 +75,7 @@ class LPIPS(nn.Module):
         for l in range(num_features):
             val += res[l]
 
-        return val
+        return val.view(-1)
 
 
 class LPIPSWithDiscriminator(nn.Module):