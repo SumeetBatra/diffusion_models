2366df1a182eeabb858803a173cab57ff9652985
Karkala Hegde <khegde@discovery2.hpc.usc.edu>
2023-04-07 03:17:48 -0700

    carc runner working

--- launch/carc_run.sh
+++ launch/carc_run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --account=gaurav_1048
 #SBATCH --partition=gpu
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:a100:1
-#SBATCH --mem=16GB
-#SBATCH --time=2:30:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=04:00:00
 #SBATCH --output=tmp/dm-%j.log
+#SBATCH --nodes=1
 
 module purge
 module load gcc/11.3.0
@@ -15,6 +14,12 @@ module load cuda/11.6.2
 module load cudnn/8.4.0.27-11.6
 eval "$(conda shell.bash hook)"
 conda activate qd
+
+su_charge=$(scontrol show job $SLURM_JOB_ID -dd | grep -oP '(?<=billing=).*(?=,gres/gpu=)')
+# log the SU charge
+echo "estimated max su charge $su_charge"
+
+num_cpus_per_task=$SLURM_CPUS_PER_TASK
 
 tags=plabl
 seed=111
@@ -31,6 +36,9 @@ seed=111
 
 pl=1
 
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
 
-srun python -m algorithm.train_autoencoder --seed $seed --use_wandb True --num_epochs 200 --merge_obsnorm False --wandb_tag $tags --inp_coef 1 --perceptual_loss $pl
-
+srun python -m algorithm.train_autoencoder --env_name halfcheetah --use_wandb True --num_epochs 200 --wandb_tag $tags --perceptual_loss $pl  --seed $((seed + 000)) &
+srun python -m algorithm.train_autoencoder --env_name halfcheetah --use_wandb True --num_epochs 200 --wandb_tag $tags --perceptual_loss $pl  --seed $((seed + 111)) &
+srun python -m algorithm.train_autoencoder --env_name halfcheetah --use_wandb True --num_epochs 200 --wandb_tag $tags --perceptual_loss $pl  --seed $((seed + 222)) &
+wait
