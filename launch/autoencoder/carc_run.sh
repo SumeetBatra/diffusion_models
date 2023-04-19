c9b77299e68b73de809bc44b03a00636a5f0ab57
hegde95 <hegdeshashank@yahoo.com>
2023-04-19 00:35:18 -0700

    updated runner

--- launch/autoencoder/carc_run.sh
+++ launch/autoencoder/carc_run.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=gaurav_1048
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=12:00:00
 #SBATCH --output=tmp/dm-%j.log
